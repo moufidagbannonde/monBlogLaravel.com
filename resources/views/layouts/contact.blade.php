84cdceaa024da4a2ec64a0d642b08e144d7f5847
Moufid Agbannonde <magbannonde@gmail.com>
2024-07-22 11:12:22 +0200

    layouts contact créé

---
+++ resources/views/layouts/contact.blade.php
@@ -0,0 +1,4 @@
+<h2>Contactez-nous !</h2>
+<div>
+    When there is no desire, all things are at peace. - Laozi
+</div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,8 @@
+<body>
+    <h1>Mon blog Laravel</h1>
+
+    <p>
+        <a href="/contact-us">Contactez-nous</a>
+    </p>
+    
+</body>
