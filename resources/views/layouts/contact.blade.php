f64066d3145657ce205a8d5c7a1c14c615835602
Moufid Agbannonde <magbannonde@gmail.com>
2024-07-22 11:23:49 +0200

    @yield @extends @section

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -1,4 +1,9 @@
+@extends('layouts.master')
+
+@section('contenu')
 <h2>Contactez-nous !</h2>
 <div>
     When there is no desire, all things are at peace. - Laozi
 </div>
+
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mon blog Laravel</title>
+</head>
+
 <body>
     <h1>Mon blog Laravel</h1>
-
     <p>
         <a href="/contact-us">Contactez-nous</a>
     </p>
-    
+    <!-- Le contenu de toutes les pages apparaîtra en bas ici -->
+    @yield('contenu')
+    <!-- Le contenu de toutes les pages apparaîtra en bas ici -->
 </body>
+
+</html>
