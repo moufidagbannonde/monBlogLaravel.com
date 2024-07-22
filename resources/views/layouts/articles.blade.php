297fb0ffb0e09c64dd63d4173a5bd31a53c7db9f
Moufid Agbannonde <magbannonde@gmail.com>
2024-07-22 13:35:37 +0200

    @if , @else

--- resources/views/layouts/articles.blade.php
+++ resources/views/layouts/articles.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.master')
-@section('title')
+<!-- @section('title')
 Articles
-@endsection
+@endsection -->
 @section('contenu')
-<h2>Articles</h2>
-<div>
-    The best way to take care of the future is to take care of the present moment. Thich Nhat Hanh
-</div>
-@foreach ($articles as $index => $article)
-    <article>
-        <h2>{{ $article["title"] }}</h2>
-        <p>{{ $article["body"] }}</p>
-    </article>
-@endforeach
+    <!-- <h2>Articles</h2>
+    <div>
+        The best way to take care of the future is to take care of the present moment. Thich Nhat Hanh
+    </div> -->
+    @if ($articles)
+        @foreach ($articles as $index => $article)
+            <article>
+                <h2>{{ $article["title"] }}</h2>
+                <p>{{ $article["body"] }}</p>
+            </article>
+        @endforeach
+        @else
+        <p>Aucun article trouvé 😥</p>
+    @endif
 <!-- <pre>{{ dd($articles) }}</pre> -->
 @endsection
